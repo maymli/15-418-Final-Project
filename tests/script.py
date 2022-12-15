bbffee6be8c315ad6127d7f02c006287c1a39f93
maymli <mayli@andrew.cmu.edu>
2022-12-15 00:04:48 -0500

    Update script.py

--- tests/script.py
+++ tests/script.py
@@ -1,6 +1,6 @@
 import random
 
-'''
+
 f1 = open("complete-5000.txt", "w")
 f1.write("5000\n")
 
@@ -8,19 +8,33 @@ for i in range(5000):
     for j in range(i + 1, 5000):
         f1.write(str(i) + " " + str(j) + "\n")
 f1.close()
-'''
 
 '''
 f1 = open("random-5000.txt", "w")
 f1.write("5000\n")
+#pairs_count = 0
+#while pairs_count < 2500000:
 for _ in range(2500000):
     first = random.randrange(0, 5000)
     second = random.randrange(0, 5000)
     if first != second:
         f1.write(str(first) + " " + str(second) + "\n")
+        # pairs_count += 1
 f1.close()
 '''
-
+'''
+f1 = open("random-50000.txt", "w")
+f1.write("50000\n")
+#pairs_count = 0
+#while pairs_count < 2500000:
+for _ in range(250000000):
+    first = random.randrange(0, 50000)
+    second = random.randrange(0, 50000)
+    if first != second:
+        f1.write(str(first) + " " + str(second) + "\n")
+        # pairs_count += 1
+f1.close()
+'''
 '''
 f1 = open("sparse-50000.txt", "w")
 f1.write("50000\n")
@@ -31,15 +45,35 @@ for _ in range(2500000):
         f1.write(str(first) + " " + str(second) + "\n")
 f1.close()
 '''
-
 '''
-f2 = open("complete-8000.txt", "w")
-f2.write("8000")
+f1 = open("sparse-50000.txt", "w")
+f1.write("50000\n")
+for _ in range(2500000):
+    first = random.randrange(0, 5000)
+    second = random.randrange(0, 5000)
+    if first != second:
+        f1.write(str(first) + " " + str(second) + "\n")
+f1.close()
+'''
+'''
+f1 = open("very-sparse-50000.txt", "w")
+f1.write("50000\n")
+for _ in range(25000):
+    first = random.randrange(0, 5000)
+    second = random.randrange(0, 5000)
+    if first != second:
+        f1.write(str(first) + " " + str(second) + "\n")
+f1.close()
+'''
+
+
+f2 = open("complete-50000.txt", "w")
+f2.write("50000")
 for i in range(8000):
     for j in range(i + 1, 8000):
         f2.write(str(i) + " " + str(j) + "\n")
 f2.close()
-'''
+
 
 '''
 f1 = open("corner-50000.txt", "w")
