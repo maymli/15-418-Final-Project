ce68aa1802b1962790032f3d22535103aaf3ff31
maymli <mayli@andrew.cmu.edu>
2022-12-17 15:23:49 -0500

    Update openmpi-coloring.cpp
    Make fixes across versions, remove bugs and unnecessary comments

--- openmpi-coloring.cpp
+++ openmpi-coloring.cpp
@@ -9,9 +9,11 @@
 #include <sstream>
 #include <string>
 #include <unordered_set>
+#include <queue>
 
 struct StartupOptions {
   std::string inputFile = "";
+  int chunk_size;
 };
 
 StartupOptions parseOptions(int argc, char **argv) {
@@ -19,6 +21,9 @@ StartupOptions parseOptions(int argc, char **argv) {
   for (int i = 1; i < argc; i++) {
     if (strcmp(argv[i], "-f") == 0) {
       so.inputFile = argv[i+1];
+    }
+    else if (strcmp (argv[i], "-chunksize") == 0) {
+      so.chunk_size = std::stoi(argv[i+1]);
     }
   }
   return so;
@@ -112,17 +117,13 @@ int firstAvailableColor(int node, std::unordered_map<graphNode, std::vector<grap
 
 void colorGraph(int start, int end, std::vector<graphNode> &nodes, std::unordered_map<graphNode, std::vector<graphNode>> &graph,
                      std::unordered_map<graphNode, color> &colors) {
-
-    // std::cerr << "coloring graph!" << "\n";
     for (int i = start; i < end; i ++) {
       auto node = nodes[i];
       int color = firstAvailableColor(node, graph, colors);
       colors[node] = color;
-      // std::cerr << "node " << node << ", color " << colors[node] << "\n";
     }
 }
 
-//
 void receiveAndCopy(std::vector<graphNode> &nodes, MPI_Status &status, const int &chunkSize, std::unordered_map<graphNode, color> &colors) {
   std::vector<int> nodeColors;
   nodeColors.resize(chunkSize);
@@ -184,59 +185,42 @@ void headProc(std::vector<graphNode> &nodes, std::unordered_map<graphNode, color
 
 void headCorrection(std::unordered_map<graphNode, std::vector<graphNode>> graph, const int numPairs, std::vector<graphNode> nodes, std::unordered_map<graphNode, color> &colors, const int &nproc,
                   const int chunkSize) {
-
-  // can maybe partition nodes (ids) and parallelize checking and correcting? for now have P0 do all though
-  int numColors = 0;
-  int maxColor = 0;
-  for (size_t i = 0; i < nodes.size(); i++) {
-      int node = nodes[i];
-      maxColor = std::max(maxColor, colors[node]);
-      // std::cerr << "node " << node << " color " << colors[node] << "\n";
-  }
-  numColors = maxColor + 1;
-  std::cerr << "num colors " << numColors << "\n";
-
+  //int wrong = 0;
   for (size_t i = 0 ; i < nodes.size(); i++) {
     int node = nodes[i];
     int color = colors[node];
 
     for (auto &nbor : graph[node]) {
         if (color == colors[nbor]) {
-          std::cerr << "(node " << node << ", " << nbor << "): color " << color << " and new " << numColors << "\n";
-          colors[node] = numColors++;
-          //std::cerr << "num colors " << numColors << "\n";
+          //wrong += 1;
+          colors[node] = firstAvailableColor(node, graph, colors);//numColors++;
           break;
         }
     }
   }
+  //std::cerr << "wrongs: " << wrong << std::endl;
 }
   
 void taskProc(std::unordered_map<graphNode, std::vector<graphNode>> &graph, std::vector<graphNode> &nodes, const int &chunkSize) {
     int chunk_i;
     MPI_Status status;
+    // find a coloring for these specific nodes
+    std::unordered_map<graphNode, color> colors;
 
     while (true) {
       MPI_Recv(&chunk_i, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
       if (status.MPI_TAG == chunkSize) {
           return;
       }
-
-      // find a coloring for these specific nodes
-      std::unordered_map<graphNode, color> colors;
-      // if (chunk_i == 0)
+      
       int start = chunk_i * chunkSize;
       int end = (chunk_i + 1) * chunkSize;
       colorGraph(start, end, nodes, graph, colors);
 
-      std::vector<int> colorsToSend; // even position is node, next odd number is color of prev even node
+      std::vector<int> colorsToSend;
       for (int i = start; i < end; i++) {
         auto node = nodes[i];
         colorsToSend.push_back(colors[node]); // color of node
-        
-        /*
-        if (chunk_i == 1)
-          std::cerr << "(node " << node << ", color " << colorsToSend[i] << "), ";
-        */
       }
 
       // send the nodes and colors back to Head 
@@ -247,6 +231,7 @@ void taskProc(std::unordered_map<graphNode, std::vector<graphNode>> &graph, std:
 
 int main(int argc, char *argv[]) {
   
+  #pragma region init
   // MPI initialization:
   int pid;
   int nproc;
@@ -258,18 +243,25 @@ int main(int argc, char *argv[]) {
   StartupOptions options = parseOptions(argc, argv);
   std::vector<graphNode> nodes;
   std::vector<std::pair<graphNode, graphNode>> pairs;
+  std::unordered_map<graphNode, color> colors;
 
   // Head reads in file values
   if (pid == 0) {
     readGraphFromFile(options.inputFile, nodes, pairs);
   }
   
-  std::unordered_map<graphNode, color> colors;
+  // Broadcast number of nodes and pairs of nodes
+  int numNodes = (int) nodes.size();
+  MPI_Bcast(&numNodes, 1, MPI_INT, 0, MPI_COMM_WORLD); 
 
-  // Start timer
-  MPI_Barrier(MPI_COMM_WORLD);
-  Timer t;
-  t.reset();
+  int numPairs = (int) pairs.size();
+  MPI_Bcast(&numPairs, 1, MPI_INT, 0, MPI_COMM_WORLD); 
+  
+  // Resize task processes' variables
+  if (pid != 0) {
+      nodes.resize(numNodes);
+      pairs.resize(numPairs);
+  }
 
   MPI_Datatype MPI_PAIR;
   MPI_Type_contiguous(2, MPI_INT, &MPI_PAIR);
@@ -279,35 +271,30 @@ int main(int argc, char *argv[]) {
   MPI_Bcast(&nodes[0], numNodes, MPI_INT, 0, MPI_COMM_WORLD);
   MPI_Bcast(&pairs[0], numPairs, MPI_PAIR, 0, MPI_COMM_WORLD);
 
-  // Resize task processes' variables
-  if (pid != 0) {
-      nodes.resize(numNodes);
-      pairs.resize(numPairs);
-  }
-  // std::cerr << "hello i am " << pid << "! i have " << numNodes << " nodes and " << numPairs << " pairs\n";
-
-  // Broadcast nodes and pairs from head to task processes
-  MPI_Bcast(&nodes[0], numNodes, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&pairs[0], numPairs, MPI_INT, 0, MPI_COMM_WORLD);
   // Now all processors know the nodes and the pairs/edges
 
   // Each processor builds the full graph
   std::unordered_map<graphNode, std::vector<graphNode>> graph;
   buildGraph(nodes, pairs, graph);
+  #pragma endregion
+  
+  // Each processor has nodes + graph now
+  // Start timer
+  MPI_Barrier(MPI_COMM_WORLD);
+  Timer t;
+  t.reset();
 
   // Start head and task processes
   // Head allocates a chunk of nodes that each task process colors
-  int chunkSize = 2500;
+  int chunkSize = options.chunk_size;
   if (pid == 0) {
       headProc(nodes, colors, nproc, chunkSize);
   }
   else {
       taskProc(graph, nodes, chunkSize);
   }
-  
-  MPI_Barrier(MPI_COMM_WORLD);
-  double totalSimulationTime = t.elapsed();
 
+  #pragma region finish
   // Return simulation time + check correctness
   if (pid == 0) {
 
@@ -315,7 +302,6 @@ int main(int argc, char *argv[]) {
     headCorrection(graph, numPairs, nodes, colors, nproc, chunkSize);
 
     // Finish timing and print information
-    printf("total simulation time: %.6fs\n", totalSimulationTime);
     double time_spent = t.elapsed();
     std::cout.setf(std::ios::fixed, std::ios::floatfield);
     std::cout.precision(5);
@@ -332,7 +318,7 @@ int main(int argc, char *argv[]) {
       std::cout << max + 1 << " colors\n"; 
     }
   }
-
   MPI_Finalize();
   return 0;
+  #pragma endregion
 }
